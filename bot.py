651dd9b482496ca2a5b2eef805bd8ebcb75bd5fc
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-11-13 01:18:52 +0300

    description and dev list

--- bot.py
+++ bot.py
@@ -4,7 +4,13 @@ import io
 import sys
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='c.',description="A Bot Made By AntonyJosph Free TNT And Sleedyak Under Work",owner_id=311970805342928896)
+bot = commands.Bot(command_prefix='c.',description="A bot under development by Antony, Sleedyak and Free TNT. Feel free to drop into the server and help with development and for support at https://discord.gg/qv9UcBh")
+
+developers = [
+        311970805342928896,
+        316586772349976586,
+        292690616285134850
+    ]
 
 @bot.event
 async def on_ready():
