f38b1ad9d940edc69bfe884bb637a0a7165ef240
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-11-13 10:28:41 -0900

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -35,7 +35,7 @@ class Mod:
         try:
             await ctx.send(f"Kicked {user.name} from the server.")
             await ctx.guild.kick(user)
-        discord.Forbidden:
+        except discord.Forbidden:
             await ctx.send("You don't have permissions to do this")
             
             
