ada702e3dbfa42035169b51d3423146429459cb3
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-11-13 03:32:07 -0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,6 +33,28 @@ async def ping(ctx):
     em.description = f'{bot.ws.latency * 1000:.4f} ms'
     await ctx.send(embed=em)
 
+@bot.command(name='presence')
+async def set(ctx, Type=None,*,thing=None):
+  if ctx.author not in developers:
+      return
+
+  if Type is None:
+    await ctx.send('Usage: `.presence [game/stream] [message]`')
+  else:
+    if Type.lower() == 'stream':
+      await bot.change_presence(game=discord.Game(name=thing,type=1,url='https://www.twitch.tv/a'),status='online')
+      await ctx.send(f'Set presence to. `Streaming {thing}`')
+    elif Type.lower() == 'game':
+      await bot.change_presence(game=discord.Game(name=thing))
+      await ctx.send(f'Set presence to `Playing {thing}`')
+    elif Type.lower() == 'clear':
+      await bot.change_presence(game=None)
+      await ctx.send('Cleared Presence')
+    else:
+      await ctx.send('Usage: `.presence [game/stream] [message]`')
+
+
+
 if not os.environ.get('TOKEN'):
   print("no token found REEEE!")
 bot.run(os.environ.get('TOKEN').strip('\"'))
