40681c83d696828ecbd8e18dd344baffdbc33317
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-11-13 09:19:47 -0900

    Create mod.py

---
+++ cogs/mod.py
@@ -0,0 +1,43 @@
+'''
+MIT License
+Copyright (c) 2017 AntonyJoseph, Free TNT, Sleedyak
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+import discord
+import os
+import io
+import sys
+from discord.ext import commands
+
+class Mod:
+    def __init__(self, bot):
+        self.bot = bot
+        
+
+    @commands.command()
+    @commands.has_permissions(kick_members = True)
+    async def kick(self, ctx, user: discord.Member)
+        try:
+            await ctx.send(f"Kicked {user.name} from the server.)
+            await user.kick()
+        except;
+            await ctx.send("You don't have permissions to do this")
+            
+            
+def setup(bot):
+    bot.add_cog(Mod(bot))
