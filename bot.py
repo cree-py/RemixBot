d6a6d2ca1b2887cd643745848503ba38f85b5ad7
Sleedyak <31696975+Sleedyak@users.noreply.github.com>
2017-11-13 21:33:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ async def help(ctx):
     em.title = "Help"
     em.description = "A bot under development by Antony, Sleedyak and Free TNT. Feel free to drop into the server and help with development and for support [here](https://discord.gg/qv9UcBh) \n"
     em.add_field(name="Ping", value="Pong!")
-    em.add_field(name="Invite", value="Invite me to your server")
+    em.add_field(name="Invite", value="Invite me to your server.")
     em.add_field(name="Kick", value="Kick someone from the server.")
     await ctx.send(embed=em)
 
