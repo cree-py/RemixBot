a3a1d7c54f3a0c7519fda6d233994ea3652528d8
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-11-13 03:40:27 -0900

    Eval is fine to copy lol

--- bot.py
+++ bot.py
@@ -12,6 +12,14 @@ developers = [
         292690616285134850
     ]
 
+def cleanup_code(content):
+    """Automatically removes code blocks from the code."""
+    # remove ```py\n```
+    if content.startswith('```') and content.endswith('```'):
+        return '\n'.join(content.split('\n')[1:-1])
+
+    return content.strip('` \n')
+
 @bot.event
 async def on_ready():
    ctx.send("Bot Is Online")
@@ -52,6 +60,55 @@ async def set(ctx, Type=None,*,thing=None):
       await ctx.send('Cleared Presence')
     else:
       await ctx.send('Usage: `.presence [game/stream] [message]`')
+
+@bot.command(pass_context=True, hidden=True, name='eval')
+@commands.is_owner()
+async def _eval(ctx, *, body: str):
+    if ctx.author not in developers:
+        return
+
+
+        env = {
+            'bot': bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+        }
+
+        env.update(globals())
+
+        body = cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                await ctx.send(f'```py\n{value}{ret}\n```')
+
 
 
 
