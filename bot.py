35bbbced77f5745db6dbc18a49595f32ffc89b79
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-11-14 12:00:39 -0900

    Warn command n help

--- bot.py
+++ bot.py
@@ -44,6 +44,7 @@ async def help(ctx):
     em.add_field(name="Mute", value="Mutes someone from a specified channel. Requires the ban members permission")
     em.add_field(name="Unmute",value="Unmute someone you previously muted. Requires the ban members permission")
     em.add_field(name="Say",value="Say something as the bot.") 
+    en.add_field(name="Warn",value="Warn a user usage: c.warn @someone reason here")
     em.add_field(name="Help", value="Shows this message.")
     await bot.get_user(ctx.message.author.id).send(embed=em)
     if ctx.message.channel.guild:
