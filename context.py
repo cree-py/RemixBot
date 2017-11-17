50b67d75a2a04b755a167fba28d25068013ab3b7
Jason <31069084+SharpBit@users.noreply.github.com>
2017-11-17 12:25:01 -0500

    Create context.py

---
+++ context.py
@@ -0,0 +1,11 @@
+import discord
+from discord.ext import commands
+import json
+import os
+
+
+class CustomContext(commands.Context):
+
+async def random_color(self, ctx):
+    '''Returns a random color'''
+    color = 
