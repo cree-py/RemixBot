36e65dacf0a9f36067c5e0fc2bf3bfbc18e0a44f
Jason <31069084+SharpBit@users.noreply.github.com>
2017-11-17 12:29:55 -0500

    Add a custom context

--- context.py
+++ context.py
@@ -5,7 +5,62 @@ import os
 
 
 class CustomContext(commands.Context):
+    '''Custom Context class to provide utility.'''
 
-async def random_color(self, ctx):
-    '''Returns a random color'''
-    color = 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @property
+    def session(self):
+        '''Returns the bot's aiohttp client session'''
+        return self.bot.session
+
+    def delete(self):
+        '''shortcut'''
+        return self.message.delete()
+
+    async def get_ban(self, name_or_id):
+        '''Helper function to retrieve a banned user'''
+        for ban in await self.guild.bans():
+            if name_or_id.isdigit():
+                if ban.user.id == int(name_or_id):
+                    return ban
+            if name_or_id.lower() in str(ban.user).lower():
+                return ban
+
+    async def purge(self, *args, **kwargs):
+        '''Shortcut to channel.purge'''
+        kwargs.setdefault('bulk', False)
+        await self.channel.purge(*args, **kwargs)
+        
+    async def random_color(self):
+        '''returns a random color (for embeds)'''
+        color = ('#%06x' % random.randint(8, 0xFFFFFF))
+        color = int(color[1:], 16)
+        color = discord.Color(value=color)
+        return color
+    
+    async def send_cmd_help(self):
+        '''Sends command help'''
+        if self.invoked_subcommand:
+            pages = self.formatter.format_help_for(self, self.invoked_subcommand)
+            for page in pages:
+                await self.send_message(self.message.channel, page)
+        else:
+            pages = self.formatter.format_help_for(self, self.command)
+            for page in pages:
+                await self.send_message(self.message.channel, page)
+                
+    @staticmethod
+    def paginate(text: str):
+        '''Simple generator that paginates text.'''
+        last = 0
+        pages = []
+        for curr in range(0, len(text)):
+            if curr % 1980 == 0:
+                pages.append(text[last:curr])
+                last = curr
+                appd_index = curr
+        if appd_index != len(text) - 1:
+            pages.append(text[last:curr])
+        return list(filter(lambda a: a != '', pages)
