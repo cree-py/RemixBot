24b187e0cba79618582116c6bfd1146fcbe5038b
umbr 3 <30674647+umbresp@users.noreply.github.com>
2017-11-18 13:38:02 -0600

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -6,15 +6,16 @@ class Fun:
     def __init__(self, bot):
         self.bot = bot
         
-    responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+
+    @commands.command(aliases='8ball')
+    async def eightball(self, ctx, question:str):
+        responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
                  "You may rely on it", "As I see it, yes", "Most likely", "Outlook good",
                  "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
                  "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
                  "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good",
                  "Very doubtful"]
 
-    @commands.command(aliases='8ball')
-    async def eightball(self, ctx, question:str):
         num = random.randint(0, 20)
         response = responses[num]
         em = discord.Embed(color=discord.Color(value=0xff0000))
