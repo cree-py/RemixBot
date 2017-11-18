5e1f5b0478ea997cec4588be5cb9c008aa350875
umbr 3 <30674647+umbresp@users.noreply.github.com>
2017-11-18 14:46:45 -0600

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -7,7 +7,7 @@ class Fun:
         self.bot = bot
         
 
-    @commands.command(aliases='8ball')
+    @commands.command(aliases=['8ball'])
     async def eightball(self, ctx, *, question:str):
         responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
                  "You may rely on it", "As I see it, yes", "Most likely", "Outlook good",
@@ -16,7 +16,7 @@ class Fun:
                  "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good",
                  "Very doubtful"]
 
-        num = random.randint(0, 20)
+        num = random.randint(0, 19)
         if num < 10:
             em = discord.Embed(color=discord.Color(value=0x00ff00))
         elif num < 15:
