02a8da196493d9a70d7bed8a69f628fafc8a5f37
umbr 3 <30674647+umbresp@users.noreply.github.com>
2017-11-18 13:40:11 -0600

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -8,7 +8,7 @@ class Fun:
         
 
     @commands.command(aliases='8ball')
-    async def eightball(self, ctx, question:str):
+    async def eightball(self, ctx, *, question:str):
         responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
                  "You may rely on it", "As I see it, yes", "Most likely", "Outlook good",
                  "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
