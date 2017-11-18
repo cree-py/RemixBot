68bc0c2bef05930be8fdd2849912c0af6b1748ba
Jason <31069084+charadox@users.noreply.github.com>
2017-11-17 20:45:01 -0500

    plain add_field

--- cogs/info.py
+++ cogs/info.py
@@ -18,24 +18,20 @@ class Info:
         color = discord.Color(value=0x00ff00)
 
         em = discord.Embed(description=created_at, color=color)
-        fields = [
-            ('Online Members', len(
-                [m for m in guild.members if discord.Status != discord.Status.offline])),
-            ('Total Members', len(guild.members)),
-            ('Text Channels', len(guild.text_channels)),
-            ('Voice Channels', len(guild.voice_channels)),
-            ('Roles', len(guild.roles)),
-            ('Owner', guild.owner)
-        ]
-        for f, v in fields:
-            em.add_field(name=f, value=v)
+        em.add_field(name='Online Members', value=len(
+            [m for m in guild.members if discord.Status != discord.Status.offline]))
+        em.add_field(name='Total Members', value=len(guild.members))
+        em.add_field(name='Text Channels', value=len(guild.text_channels))
+        em.add_field(name='Voice Channels', value=len(guild.voice_channels))
+        em.add_field(name='Roles', value=len(guild.roles))
+        em.add_field(name='Owner', value=guild.owner)
 
         em.set_thumbnail(url=None or guild.icon_url)
         em.set_author(name=guild.name, icon_url=None or guild.icon_url)
         await ctx.send(embed=em)
 
     @commands.command(aliases=['ui', 'user'])
-    async def userinfo(self, ctx, user: discord.Member=None):
+    async def userinfo(self, ctx, user: discord.Member = None):
         '''Get user info for yourself or someone in the guild'''
         user = user or ctx.message.author
         guild = ctx.message.guild
@@ -54,16 +50,11 @@ class Info:
         desc = f'{user.name} is chilling in {user.status} mode.'
         member_number = sorted(guild.members,  key=lambda m: m.joined_at).index(user) + 1
         em = discord.Embed(color=color, description=desc, timestamp=time)
-
-        fields = [
-            ('Name', user.name),
-            ('Member Number', member_number),
-            ('Account Created', user.created_at.__format__('%A, %B %d, %Y')),
-            ('Join Date', user.joined_at.__format__('%A, %B %d, %Y')),
-            ('Roles', rolenames)
-        ]
-        for f, v in fields:
-            em.add_field(name=f, value=v)
+        em.add_field(name='Name', value=user.name),
+        em.add_field(name='Member Number', value=member_number),
+        em.add_field(name='Account Created', value=user.created_at.__format__('%A, %B %d, %Y')),
+        em.add_field(name='Join Date', value=user.joined_at.__format__('%A, %B %d, %Y')),
+        em.add_field(name='Roles', value=rolenames)
         em.set_thumbnail(avi)
         await ctx.send(embed=em)
 
