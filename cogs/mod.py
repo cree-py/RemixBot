f06262bae5dad1abc8cfce72a3aac4ea731fe937
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-11-18 21:07:27 -0500

    add license to code at top, allow people ti choose a min and max number for random number command

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,6 +1,6 @@
 '''
 MIT License
-Copyright (c) 2017 AntonyJoseph, Free TNT, Sleedyak, Victini, SharpBit
+Copyright (c) 2017 Cree-Py
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -19,85 +19,81 @@ SOFTWARE.
 '''
 
 import discord
-import os
-import io
-import sys
 from discord.ext import commands
+
 
 class Mod:
     def __init__(self, bot):
         self.bot = bot
-        
 
     # DO NOT change the error messages again
     # The @commands check eliminates user perms
     # So the only possible way is for the bot to
     # not have perms.
     # The old error message is inaccurate.
-    
+
     @commands.command()
-    @commands.has_permissions(kick_members = True)
+    @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, user: discord.Member):
         '''Kick a member from the guild'''
-        try: 
+        try:
             await ctx.guild.kick(user)
             await ctx.send(f"Kicked {user.name} from the server.")
         except discord.Forbidden:
             await ctx.send("I could not kick the user. Make sure I have the kick members permission.")
-    
+
     @commands.command()
-    @commands.has_permissions(ban_members = True)
+    @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, user: discord.Member):
-       '''Ban a member from the guild'''
-       try:
-           await ctx.guild.ban(user)
-           await ctx.send(f"Banned {user.name} from the server.")
-       except discord.Forbidden:
-           await ctx.send("I could not ban the user. Make sure I have the ban members permission.")
-            
+        '''Ban a member from the guild'''
+        try:
+            await ctx.guild.ban(user)
+            await ctx.send(f"Banned {user.name} from the server.")
+        except discord.Forbidden:
+            await ctx.send("I could not ban the user. Make sure I have the ban members permission.")
+
     @commands.command()
-    @commands.has_permissions(ban_members = True)
+    @commands.has_permissions(ban_members=True)
     async def mute(self, ctx, user: discord.Member):
         '''Mute a member in the channel'''
         try:
-            await ctx.channel.set_permissions(user, send_messages = False)
+            await ctx.channel.set_permissions(user, send_messages=False)
             await ctx.channel.send(f"{user.mention} has been muted from this channel")
         except discord.Forbidden:
             await ctx.send("I could not unmute the user. Make sure I have the manage channels permission.")
 
     @commands.command()
-    @commands.has_permissions(ban_members = True)
+    @commands.has_permissions(ban_members=True)
     async def unmute(self, ctx, user: discord.Member):
         '''Unmute a member from the channel'''
         try:
-            await ctx.channel.set_permissions(user, send_messages = True)
+            await ctx.channel.set_permissions(user, send_messages=True)
             await ctx.channel.send(f"{user.mention} has been unmuted from this channel.")
         except discord.Forbidden:
             await ctx.send("I could not unmute the user. Make sure I have the manage channels permission.")
-        
+
     @commands.command()
-    @commands.has_permissions(kick_members = True)
-    async def warn(self, ctx, user: discord.Member, *, reason:str):
+    @commands.has_permissions(kick_members=True)
+    async def warn(self, ctx, user: discord.Member, *, reason: str):
         '''Warn a member via DMs'''
         warning = f"You have been warned in **{ctx.message.guild}** by **{ctx.message.author}** for {reason}"
         if not reason:
             warning = f"You have been warned in **{ctx.message.guild}** by **{ctx.message.author}**"
         await user.send(warning)
         await ctx.send(f"**{user}** has been **warned**")
-        
+
     @commands.command()
-    @commands.has_permissions(manage_messages = True)
-    async def purge(self, ctx, messages:int):
+    @commands.has_permissions(manage_messages=True)
+    async def purge(self, ctx, messages: int):
         '''Delete messages'''
         if messages > 99:
             messages == 99
-        
+
         try:
             ctx.delete_messages(messages)
         except:
             await ctx.send("I cannot delete the messages. Make sure I have the manage messages permission.")
-                       
-            
+
 
 def setup(bot):
-        bot.add_cog(Mod(bot))
+    bot.add_cog(Mod(bot))
