677fa2edee5120456a7f720859bf2e2013fe2999
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-11-23 17:03:45 -0500

    minor fixes

--- cogs/clashroyale.py
+++ cogs/clashroyale.py
@@ -1,14 +1,18 @@
 '''
 MIT License
+
 Copyright (c) 2017 Cree-Py
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,31 +25,30 @@ import discord
 from discord.ext import commands
 import crasync
 
+
 class ClashRoyale:
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
         self.client = crasync.Client()
-    
+
     @commands.command()
     async def profile(self, ctx, tag=None):
         '''Fetch a Clash Royale Profile by tag'''
         em = discord.Embed(title="Profile", color=discord.Color(value=0x00ff00))
-        if tag == None:
+        if tag is None:
             em.description = "Please enter a clash royale tag.\nExample: `c.profile #22UP0G0YU`"
             return await ctx.send(embed=em)
-        tag = tag.strip('#').replace('O' ,'0')
+        tag = tag.strip('#').replace('O', '0')
         try:
             profile = await self.client.get_profile(tag)
         except Exception as e:
             return await ctx.send(f'`{e}`')
-        
-        
-        
+
         em.title = profile.name
         em.set_thumbnail(url=profile.arena.image_url)
         em.description = f"#{tag}"
         em.url = f"http://cr-api.com/profile/{tag}"
-        em.add_field(name="Current Trophies",value=f"{profile.current_trophies}")
+        em.add_field(name="Current Trophies", value=f"{profile.current_trophies}")
         em.add_field(name="Highest Trophies", value=f"{profile.highest_trophies}")
         em.add_field(name="Legend Trophies", value=f"{profile.legend_trophies}")
         em.add_field(name="Level", value=f"{profile.level}")
@@ -63,15 +66,14 @@ class ClashRoyale:
             em.set_author(name="Profile", icon_url=profile.clan_badge_url)
         except:
             em.set_author(name="Profile")
-            
-       
+
         await ctx.send(embed=em)
-        
+
     @commands.command()
     async def clan(self, ctx, tag=None):
         '''Gets Clan info by clan tag.'''
         em = discord.Embed(title='Clan Info', color=discord.Color(value=0x00ff00))
-        if tag == None:
+        if tag is None:
             em.description = "Please enter a clan tag.\n Example: `c.clan #29UQQ282`"
             return await ctx.send(embed=em)
         tag = tag.strip('#').replace('O', '0')
@@ -79,8 +81,7 @@ class ClashRoyale:
             clan = await self.client.get_clan(tag)
         except Exception as e:
             return await ctx.send(f'`{e}`')
-            return await ctx.send(embed=em)
-        
+
         em.set_author(name="Clan Info", icon_url=clan.badge_url or None)
         em.title(f"{clan.name} (#{clan.tag})")
         em.set_thumbnail(url=clan.badge_url)
@@ -96,6 +97,6 @@ class ClashRoyale:
         em.set_footer(text="Powered by cr-api.com", icon_url="http://cr-api.com/static/img/branding/cr-api-logo.png")
         await ctx.send(embed=em)
 
+
 def setup(bot):
     bot.add_cog(ClashRoyale(bot))
-    
