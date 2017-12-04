f4c392c2b98b61701a0949646ecf778d1896e74f
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-12-04 17:19:45 -0500

    tnt every bot. u made is supposed to be self.bot

--- cogs/clashofclans.py
+++ cogs/clashofclans.py
@@ -1,14 +1,18 @@
 '''
 MIT License
+
 Copyright (c) 2017 Cree-py
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,40 +25,41 @@ SOFTWARE.
 
 import requests
 import json
-import os
-import io
 import discord
 from discord.ext import commands
+
 
 class ClashOfClans:
     def __init__(self, bot):
         self.bot = bot
-    
-    
+
     @commands.command()
     async def cocprofile(self, ctx, tag=None):
         '''Get a Clash of Clans profile by tag'''
         em = discord.Embed(color=discord.Color(value=0x00ff00))
-        if tag == None:
+        if tag is None:
             em.description = "Please enter a tag. For example, c.cocprofile #92CP9Y8PC"
             return await ctx.send(embed=em)
         tag = tag.strip('#')
+        with open('data/cocapi.json') as f:
+            coc = json.load(f)
+            apikey = coc.get('APIKEY')
         try:
-            headers={'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQ3Y2UzZTE3LWQxYmItNDY1MS05YjBiLWQyOWRlMWVhNWIyNCIsImlhdCI6MTUxMjM5NDI2MCwic3ViIjoiZGV2ZWxvcGVyLzM2ZTA0MDAwLTgyMmQtMDk3My1kZjBlLWI4MTZlNTBhMTVlZiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjM0LjIyNy4xNTAuNDUiXSwidHlwZSI6ImNsaWVudCJ9XX0.NwQOk9rg0D7JexC3Ol-JEKL1nMvdeERXgvIMIm2UhBwsyE01jxESYaVdxujGm_WAdBhOJAOYC47dmkjo046YAQ"}
+            headers = {'Authorization': apikey}
             response = requests.get('https://api.clashofclans.com/v1/players/%23{tag}', headers=headers)
         except:
-             em.description = "Invalid Tag. Please check the tag that you entered is correct"
-             return await ctx.send(embed=em)
+            em.description = "Invalid Tag. Please check the tag that you entered is correct"
+            return await ctx.send(embed=em)
 
-        status  = response.status_code
+        status = response.status_code
         name = response.json()['name']
-        clanname=response.json()['clan']['name']
-        clan_emoji = bot.get_emoji(387281156258922508)
-        trophy_emoji = bot.get_emoji(387281233106698241)
-        defense = bot.get_emoji(387281145320046592)
-        exp = bot.get_emoji(387315278427717643)
-        pb = bot.get_emoji(387281346898165761)
-        cc = bot.get_emoji(387281270960422922)
+        clanname = response.json()['clan']['name']
+        clan_emoji = self.bot.get_emoji(387281156258922508)
+        trophy_emoji = self.bot.get_emoji(387281233106698241)
+        defense = self.bot.get_emoji(387281145320046592)
+        exp = self.bot.get_emoji(387315278427717643)
+        pb = self.bot.get_emoji(387281346898165761)
+        cc = self.bot.get_emoji(387281270960422922)
         em.title = "CoC Profile"
         em.description = "Clash of Clans Stats: **BETA**"
         em.set_thumbnail(url=response.json()['league']['iconUrls']['medium'])
@@ -70,6 +75,7 @@ class ClashOfClans:
         em.add_field(name="Clan Role", value=response.json()['role'].title())
         em.set_footer(text="Stats by Cree-Py with CoC API")
         await ctx.send(embed=em)
-    
+
+
 def setup(bot):
     bot.add_cog(ClashOfClans(bot))
