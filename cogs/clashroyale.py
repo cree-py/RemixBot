127a23072c66cef42f5fb1f3c2b0598930f240fc
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-12-06 18:20:49 -0500

    fix quote thing

--- cogs/clashroyale.py
+++ cogs/clashroyale.py
@@ -154,7 +154,7 @@ class Clash_Royale:
 
         deck = ''
         for card in profile.deck:
-            deck += f'{self.bot.get_emoji(self.emoji(card.name.lower().strip('.').strip(' - ').strip(' ')))}{card.level}\n'
+            deck += f"{self.bot.get_emoji(self.emoji(card.name.lower().strip('.').strip('-').strip(' ')))}{card.level}"
 
         em.title = profile.name
         em.description = f'#{tag}'
