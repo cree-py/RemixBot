bf4f425d0259d1730a66ffe1f845bed3484f8438
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-12-28 09:33:05 -0500

    no results error handling

--- cogs/utility.py
+++ cogs/utility.py
@@ -120,10 +120,14 @@ class Utility:
         '''Search terms with urbandictionary.com'''
         em = discord.Embed(title=f'{query}', color=0x00ff00)
         em.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        defs = ud.define(query)
-        res = defs[0]
-        em.description = f'**Definition:** {res.definition}\n**Usage:** {res.example}\n**Votes:** {res.upvotes}:thumbsup:{res.downvotes}:thumbsdown:'
         em.set_footer(text='Powered by urbandictionary.com')
+        defs = ud.define(query)
+        try:
+            res = defs[0]
+        except IndexError:
+            em.description = 'No results.'
+            return await ctx.send(embed=em)
+        em.description = f'**Definition:** {res.definition}\n**Usage:** {res.example}\n**Votes:** {res.upvotes}:thumbsup:{res.downvotes}:thumbsdown:'
         await ctx.send(embed=em)
 
     @commands.group(invoke_without_command=True)
