594b67c5b7c1b40612debe4bad174650192dbbd5
the fixer of typos <30674647+umbresp@users.noreply.github.com>
2018-01-02 15:53:07 -0600

    Update pokedex.py

--- cogs/pokedex.py
+++ cogs/pokedex.py
@@ -84,6 +84,55 @@ class Pokedex:
                         pass       
                 em.description=description
         await ctx.send(embed=em)
+        
+    @pokemon.command()
+    async def info(self, ctx, pokemon):
+        '''Get stats about a random pokemon.'''
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon}/') as resp:
+                    data = await resp.json()
+                    id = data['id']
+                    em = discord.Embed(color=discord.Color(value=0x00FF00))
+                    em.title = data['name'].title()
+                    em.set_thumbnail(url=data['sprites']['front_default'])
+                    em.add_field(name="Height", value=str(data['height']/10) + ' meters')
+                    em.add_field(name="Weight", value=str(data['weight']/10) + ' kilograms')
+                    j = 0
+                    abilities = ""
+                    for i in range(len(data['abilities'])):
+                        abilities += data['abilities'][i]['ability']['name'].title().replace('-', ' ') + "\n"
+                        j += 1
+                    if j == 1:
+                        em.add_field(name="Ability", value=abilities)
+                    else:
+                        em.add_field(name="Abilities", value=abilities)
+                    types = ""
+                    j = 0
+                    for i in range(len(data['types'])):
+                        types += data['types'][i]['type']['name'].title().replace('-', ' ') + "\n"
+                        j += 1
+                    if j == 1:
+                        em.add_field(name="Type", value=types)
+                    else:
+                        em.add_field(name="Types", value=types)
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://pokeapi.co/api/v2/pokedex/national/') as resp:
+                    data = await resp.json()
+                    pokedex = data['pokemon_entries'][id-1]['pokemon_species']['url']
+            async with aiohttp.ClientSession() as session:
+                async with session.get(pokedex) as resp:
+                    data = await resp.json()
+                    for i in range(len(data['flavor_text_entries'])):
+                        if data['flavor_text_entries'][i]['language']['name'] == "en":
+                            description = data['flavor_text_entries'][i]['flavor_text']
+                            break
+                        else:
+                            pass       
+                    em.description=description
+            await ctx.send(embed=em)
+        except:
+            await ctx.send("That is not a valid pokemon name or pokedex number. Please check your spelling or note that no Gen 7 pokemon are included in pokeapi.")
 
 def setup(bot):
     bot.add_cog(Pokedex(bot))
