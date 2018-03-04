6b371f5815c246d01286f5f365e81e3a43afaab0
freetnt5852 <mohammed.ahmed.minecraft@gmail.com>
2018-03-04 20:24:38 +0300

    Canvas

---
+++ cogs/canvas.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+class Canvas:
+    '''Some fun canvas/images commands'''
+    def __init__(self, bot);
+        self.bot = bot
+    
+    @commands.command()
+    async def triggered(self, ctx, user: discord.Member=None):
+        '''Somebody is triggered'''
+        if user is None:
+            user = ctx.author
+        av = user.avatar_url + "?size=2048" if user.avatar_url.endswith(".gif") else user.avatar_url
+        await ctx.send(file=discord.File(await self.bot.api.triggered(av), "triggered.gif"))
+        
+        
+def setup(bot):
+    bot.add_cog(Canvas(bot))
+        
